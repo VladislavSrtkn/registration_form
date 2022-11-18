615b7d392dd659854a2371dc2522bf5da997c8bd
VladislavSrtkn <vladislavsrtkn@gmail.com>
2022-11-18 19:31:11 +0100

    tel input flags correct

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
 
-mix.sass('source/css/custom.scss', 'dist/css/style.css');
+mix.sass('source/css/custom.scss', 'dist/css/style.css').options({
+  processCssUrls: false,
+});
 
 mix.js('source/js/scripts.js', 'dist/js/index.js');
 mix.js('source/js/validation_functions.js', 'dist/js/index.js');
